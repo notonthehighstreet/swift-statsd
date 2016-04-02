933c48f530a3cd3a0ed1f8e9f676da07d8daf871
Nic Jackson <jackson.nic@gmail.com>
2016-04-02 12:07:31 +0100

    added timer to send data to statsD on 1 second intervals

--- Sources/StatsD.swift
+++ Sources/StatsD.swift
@@ -1,20 +1,34 @@
+import Foundation
+
 public class StatsD
 {
 
   var host:String
   var port:Int
   var socket:Socket
-
+  var sendCallback:(() -> Void)?
   var counters = [String: Int32]()
+  var timer:NSTimer?
 
-  init(host:String, port:Int, socket: Socket) {
+  // optional sendCallback is a closure which is called whenever the class sends data to the statsD server
+  // can be used for testing or logging.
+  init(host:String, port:Int, socket: Socket, sendCallback: (() -> Void)? = nil) {
     self.socket = socket
     self.port = port
     self.host = host
+    self.sendCallback = sendCallback
+
+    self.timer = NSTimer.scheduledTimer(1, repeats: true) { (timer: NSTimer) -> Void in
+      self.sendCounters()
+    }
+  }
+
+  // Must be called when finished with the client or the timer will never shut down
+  public func dispose() {
+    self.timer!.invalidate()
   }
 
   public func increment(bucket:String) {
-    //gorets:1|c
     if counters[bucket] != nil {
       counters[bucket]! += 1
     } else {
@@ -25,8 +39,15 @@ public class StatsD
   // This is not the most efficient way to do this, multiple counts can be concatonated and sent
   private func sendCounters() {
     for (bucket, count) in counters {
+      //format [bucket]:[count]|c
       let data = bucket + ":" + String(count) + "|c"
       send(data)
+    }
+
+    counters = [String: Int32]()
+
+    if sendCallback != nil {
+      sendCallback!()
     }
   }
 
--- Tests/StatsD/StatsDTests.swift
+++ Tests/StatsD/StatsDTests.swift
@@ -3,11 +3,21 @@ import XCTest
 
 @testable import StatsD
 
+class MockSocket :Socket {
+  var timesWritten = 0
+  var dataWritten = [String]()
+
+  internal func write(host: String, port: Int, data: String) -> (Bool, SocketError?) {
+    timesWritten += 1
+    return (true, nil)
+  }
+}
+
 class StatsDTests: XCTestCase {
   func testInitSetsCorrectValues() {
     let port = 8125
     let host = "127.0.0.1"
-    let socket:Socket = UDPSocket()
+    let socket:Socket = MockSocket()
     let statsD = StatsD(host: host, port: port, socket: socket)
 
     XCTAssertEqual(port, statsD.port, "Port should be equal")
@@ -16,18 +26,60 @@ class StatsDTests: XCTestCase {
   }
 
   func testIncrementShouldIncreaseBucketCountByOne() {
-    let statsD = StatsD(host: "192.168.99.100", port: 8125, socket: UDPSocket())
+    let statsD = StatsD(host: "192.168.99.100", port: 8125, socket: MockSocket())
     statsD.increment("mybucket")
 
     XCTAssertEqual(1, statsD.counters["mybucket"], "Counter for mybucket should be 1")
   }
 
   func testIncrementTwiceShouldIncreaseBucketCountByTwo() {
-    let statsD = StatsD(host: "192.168.99.100", port: 8125, socket: UDPSocket())
+    let statsD = StatsD(host: "192.168.99.100", port: 8125, socket: MockSocket())
     statsD.increment("mybucket")
     statsD.increment("mybucket")
 
     XCTAssertEqual(2, statsD.counters["mybucket"], "Counter for mybucket should be 2")
+  }
+
+  func testSendsDataAfterInterval() {
+    let mockSocket = MockSocket()
+    let expectation = expectationWithDescription("Send data after interval")
+    let statsD = StatsD(host: "192.168.99.100", port: 8125, socket: mockSocket) {
+      XCTAssertEqual(1, mockSocket.timesWritten, "Expected to have called write")
+      expectation.fulfill()
+    }
+
+    defer {
+      statsD.dispose()
+    }
+
+    statsD.increment("mybucket")
+
+    waitForExpectationsWithTimeout(2) { error in
+      if let error = error {
+        print("Error: \(error.localizedDescription)")
+      }
+    }
+  }
+
+  func testEmptiesBucketAfterSend() {
+    let expectation = expectationWithDescription("Empty bucket after send")
+    var statsD: StatsD?
+    statsD = StatsD(host: "192.168.99.100", port: 8125, socket: MockSocket()) {
+      XCTAssertEqual(0, statsD!.counters.count, "Expected to have emptied bucket")
+      expectation.fulfill()
+    }
+
+    defer {
+      statsD!.dispose()
+    }
+
+    statsD!.increment("mybucket")
+
+    waitForExpectationsWithTimeout(2) { error in
+      if let error = error {
+        print("Error: \(error.localizedDescription)")
+      }
+    }
   }
 }
 
@@ -36,6 +88,8 @@ extension StatsDTests {
         return [
           ("testInitSetsCorrectValues", testInitSetsCorrectValues),
           ("testIncrementShouldIncreaseBucketCountByOne", testIncrementShouldIncreaseBucketCountByOne),
+          ("testSendsDataAfterInterval", testSendsDataAfterInterval),
+          ("testEmptiesBucketAfterSend", testEmptiesBucketAfterSend)
         ]
     }
 }
