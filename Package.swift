4de2df2c83358d50de5c0a5eed8bb28015c88728
Nic Jackson <jackson.nic@gmail.com>
2016-04-04 21:14:10 +0100

    excluded tests from package

--- Package.swift
+++ Package.swift
@@ -2,6 +2,6 @@ import PackageDescription
 
 let package = Package(
 	name: "StatsD",
-	targets: [Target(name: "StatsD")]
-	//exclude: ["Tests"]
+	targets: [Target(name: "StatsD")],
+	exclude: ["Tests"]
 )
